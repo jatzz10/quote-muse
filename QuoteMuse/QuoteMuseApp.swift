a8974710fd87c8c63cd2e9703c0ac2d70e464f30
jatzz10 <jatinsharma2751997@gmail.com>
2024-09-05 00:40:17 +0530

    Initial Commit

---
+++ QuoteMuse/QuoteMuseApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteMuseApp.swift
+//  QuoteMuse
+//
+//  Created by Jatin Sharma on 05/09/24.
+//
+
+import SwiftUI
+
+@main
+struct QuoteMuseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
