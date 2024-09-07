4cb23fc64606365aebe9fa0788c6b10d2a68cd5b
jatzz10 <jatinsharma2751997@gmail.com>
2024-09-07 19:11:41 +0530

    Initial practice tutorial

--- QuoteMuse/ContentView.swift
+++ QuoteMuse/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+
+    @State private var selected = "Archery"
+    @State private var id = 1
+    
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("QuoteMuse")
+                .font(.largeTitle.bold())
+            
+            Spacer()
+            
+            VStack {
+                Circle()
+                    .fill(colors.randomElement() ?? .blue)
+                    .padding()
+                    .overlay(
+                        Image(systemName: "figure.\(selected.lowercased())")
+                            .font(.system(size: 144))
+                            .foregroundColor(.white)
+                        )
+                Text("\(selected)!")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            }
+            .transition(.slide)
+            .id(id)
+            
+            Spacer()
+            
+            Button("Try Again") {
+                withAnimation(.easeOut(duration: 1)) {
+                    selected = activities.randomElement() ?? "Archery"
+                    id += 1
+                }
+            }
+            .buttonStyle(.borderedProminent)
         }
-        .padding()
     }
 }
 
